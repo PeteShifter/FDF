3c6f1f64701f0caa2aa4e6fbffb0dccb71e508cc
Pete Shifter <pete_shifter@yahoo.fr>
2020-06-02 22:42:19 +0200

    data

---
+++ src/app/data/ingredients.ts
@@ -0,0 +1,123 @@
+export const INGREDIENTS = [{
+  id: 1,
+  name: "beurre",
+  unity: "pièce"
+},
+{
+  id: 2,
+  name: "lait",
+  unity: "litre"
+},
+{
+  id: 3,
+  name: "sucre en morceaux",
+  unity: "kilo"
+},
+{
+  id: 4,
+  name: "sucre en poudre",
+  unity: "kilo"
+},
+{
+  id: 5,
+  name: "café moulu arabica",
+  unity: "paquet"
+},
+{
+  id: 6,
+  name: "yaourt",
+  unity: "pot"
+},
+{
+  id: 7,
+  name: "confiture de fraises",
+  unity: "pot"
+},
+{
+  id: 8,
+  name: "confiture d'abricots",
+  unity: "pot"
+},
+{
+  id: 9,
+  name: "confiture de framboises",
+  unity: "pot"
+},
+{
+  id: 10,
+  name: "pâte à tartiner Nutella",
+  unity: "pot de 250g"
+},
+{
+  id: 11,
+  name: "tomate",
+  unity: "pièce"
+},
+{
+  id: 12,
+  name: "concombre",
+  unity: "pièce"
+},
+{
+  id: 13,
+  name: "courgette",
+  unity: "pièce"
+},
+{
+  id: 14,
+  name: "aubergine",
+  unity: "pièce"
+},
+{
+  id: 15,
+  name: "carotte",
+  unity: "pièce"
+},
+{
+  id: 16,
+  name: "radis",
+  unity: "botte"
+},
+{
+  id: 17,
+  name: "orange",
+  unity: "pièce"
+},
+{
+  id: 18,
+  name: "pamplemousse",
+  unity: "pièce"
+},
+{
+  id: 19,
+  name: "citron",
+  unity: "pièce"
+},
+{
+  id: 20,
+  name: "fraise",
+  unity: "barquette 250g"
+},
+{
+  id: 21,
+  name: "pêche",
+  unity: "pièce"
+},
+{
+  id: 22,
+  name: "abricot",
+  unity: "pièce"
+},
+{
+  id: 23,
+  name: "prune",
+  unity: "pièce"
+},
+{
+  id: 24,
+  name: "banane",
+  unity: "pièce"
+}]
+  
+  
+  
---
+++ src/app/data/members.ts
@@ -0,0 +1,51 @@
+export const MEMBERS = [{
+  id: 1,
+  name: "Vanessa Cazabonne",
+  gender: "F",
+  age: 35,
+  streetNmr: "42",
+  streetName: "rue de la paix",
+  buildingName: "",
+  cityName: "Paris",
+  fridge: [],
+  cart: []
+},
+{
+  id: 2,
+  name: "Boris Mihajlovic",
+  gender: "M",
+  age: 31,
+  streetNmr: "27",
+  streetName: "avenue des Champs-Elysées",
+  buildingName: "",
+  cityName: "Paris",
+  fridge: [],
+  cart: []
+},
+{
+  id: 3,
+  name: "Mathias Pascual",
+  gender: "M",
+  age: 39,
+  streetNmr: "55",
+  streetName: "rue du faubourg Saint-Honoré",
+  buildingName: "Palais de l'Elysée",
+  cityName: "Paris",
+  fridge: [],
+  cart: []
+},
+{
+  id: 4,
+  name: "Johann Marais",
+  gender: "M",
+  age: 45,
+  streetNmr: "36",
+  streetName: "rue Victor Massé",
+  buildingName: "",
+  cityName: "Paris",
+  fridge: [],
+  cart: []
+}]
+  
+  
+  
